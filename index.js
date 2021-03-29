66188be8390991ed1ee41b68fc6572b1f26f7297
Ian Marshall <imarshall14@gmail.com>
2021-03-29 14:30:57 -0400

    commit tree 399be98ef7e4d26701b3fffb55a193418cc72382

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 1000) +1
+let num3 = 16
+let num4 = 6
+
+const mod = num3 % num4
+
+const max = Math.max(1,2,3,4,5,6,7,8,9,20)
